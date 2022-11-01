c0480cf83cb3952f2f7533c7b422327b0c92b7ff
Ing. Ebu Celik <ebucelik1@hotmail.com>
2022-11-01 19:03:21 +0100

    Initial Commit

---
+++ Smart Goods/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smart Goods
+//
+//  Created by Ing. Ebu Celik, BSc on 01.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
